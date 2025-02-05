9e1344444e501d86eea59609b7eadb8c0ca401da
דן קופליוביץ <dani@h-MacBook-Pro-sl-dn.local>
2025-02-05 09:20:43 -0500

    Fixed syntax error in import

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 CallbackContext
 import os
 from dotenv import load_dotenv
